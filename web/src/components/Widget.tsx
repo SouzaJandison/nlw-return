a9d5f299eb3d5516137691c2edf7238f78f64c55
SouzaJandison <jandsonbarbosa.dev@gmail.com>
2022-05-05 23:51:44 -0300

    chore: add eslint and commit linter

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,23 +1,19 @@
-import { ChatTeardropDots } from 'phosphor-react'
-import { Popover } from '@headlessui/react'
+import { Popover } from '@headlessui/react';
+import { ChatTeardropDots } from 'phosphor-react';
 
 export function Widget() {
   return (
     <Popover className="absolute bottom-5 right-5">
       <Popover.Panel>Hello World</Popover.Panel>
 
-      <Popover.Button 
-        className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'
-      >
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots size={24} />
 
-        <span 
-          className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'
-        >
-          <span className="pl-2"></span>
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2" />
           Feedback
         </span>
       </Popover.Button>
     </Popover>
-  )
+  );
 }
